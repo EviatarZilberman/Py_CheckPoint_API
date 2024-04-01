ab6e1f6472d410193deef4bb77735e45c333a6cb
Eviatar Zilberman <esztbk@gmail.com>
2024-04-01 22:28:54 +0300

    edit_payment_details started.

--- App_API.py
+++ App_API.py
@@ -1,11 +1,5 @@
-# import base64
-# from datetime import datetime, timedelta
-from flask import Flask, request, jsonify #, make_response
-# import Global_methods
+from flask import Flask, request, jsonify
 from MongoDbManager import MongoDbSingleton
-# from Enums.CookiesClaims import CookiesClaims
-# from os import environ
-# from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 
 
@@ -62,23 +56,6 @@ def login():
     from DataModels.User import User
     user = User.from_dict(dictionary)
     if password == user.m_password:
-        # claims = list()
-        # claims.append(str(CookiesClaims.USERNAME))
-        # claims.append(str(CookiesClaims.EMAIL))
-        # parameters = list()
-        # parameters.append(user.m_username)
-        # parameters.append(user.m_email)
-        # if not stay_logged:
-        #     expiration_time = datetime.now() + timedelta(minutes = int(environ.get('EXPIRATION_TIME_DEFAULT_MINUTES')))
-        # else:
-        #     expiration_time = datetime.now() + timedelta(days = int(environ.get('EXPIRATION_TIME_PERSIST_DAYS')))
-        #     claims.append(str(CookiesClaims.IS_PERSIST))
-        #     parameters.append(str(stay_logged))
-        #
-        # expires_timestamp = expiration_time.timestamp()
-        # response = make_response()
-        # for claim, parameter in zip(claims, parameters):
-        #     response.set_cookie(str(claim), str(parameter), expires=expires_timestamp)
         result = {
             'user': user.to_dict(),
             'stay_logged': stay_logged
@@ -89,13 +66,15 @@ def login():
     return jsonify(["Not matched username, email or password!"]), 401
 
 
-# @app_api.route("/already_logged", methods = ["POST"])
-# def already_logged():
-#     data = request.get_json()
-#     email = data["email"]
-#     db_manager = MongoDbSingleton.MongoDbSingleton("E_Commerce", "Users")
-#     user_dict = db_manager.find_one_by_key_value("email", email)
-#     return jsonify(user_dict), 200
+@app_api.route("/edit_payment_details", methods = ['POST'])
+def edit_payment_details():
+    data = request.get_json()
+    username_or_email = data["username_or_email"]
+    password = data["password"]
+    stay_logged = data.get("stay_logged")
+
+    db_manager = MongoDbSingleton.MongoDbSingleton("E_Commerce", "Users")
+    dictionary = db_manager.find_one_by_key_value("email", username_or_email)
 
 
 if __name__ == "__main__":
