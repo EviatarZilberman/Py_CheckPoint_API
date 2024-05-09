f28eb23135e9ac1cefce7333e05d10590929c0af
Eviatar Zilberman <esztbk@gmail.com>
2024-05-10 00:27:45 +0300

    search and add to cart function well. problem in the logic of display of the user's cart.

--- App_API.py
+++ App_API.py
@@ -3,16 +3,19 @@ from DataModels.Address import Address
 from DataModels.PaymentDetails import PaymentDetails
 from DataModels.PersonalDetails import PersonalDetails
 from DataModels.User import User
+from Enums.LogLevels import LogLevel
 from MongoDbManager.MongoDbSingleton import MongoDbSingleton
+from Models.Writer import Writer
 
 app_api = Flask(__name__)
+path = r'C:\Users\User\Projects\Logs'
+log_file = 'CheckPoint_Logs.log'
 
 
 @app_api.route("/register", methods = ["POST"])
 def register():
     from DataModels.User import User
 
-    db_manager = MongoDbSingleton("E_Commerce", "Users")
     errors = []
     data = request.get_json()
     username = data["username"]
@@ -23,9 +26,8 @@ def register():
     confirm_password = data["confirm_password"]
 
     try:
-        user = db_manager.find_by_key_value("email", email)
+        user = MongoDbSingleton("E_Commerce", "Users").find_by_key_value("email", email)
     except Exception as e:
-        print(str(e))
         return None
 
     if user:
@@ -37,7 +39,8 @@ def register():
         return jsonify(errors), 409
 
     try:
-        db_manager.insert(User(username, f_name, l_name, email, password))
+        MongoDbSingleton.reinitialize()
+        MongoDbSingleton("E_Commerce", "Users").insert(User(username, f_name, l_name, email, password))
     except Exception as e:
         print(str(e))
         return None
@@ -52,14 +55,15 @@ def login():
     password = data["password"]
     stay_logged = data.get("stay_logged")
 
-    db_manager = MongoDbSingleton('E_Commerce', 'Users')
+    MongoDbSingleton.reinitialize()
     try:
-        dictionary = db_manager.find_one_by_key_value("email", username_or_email)
+        dictionary = MongoDbSingleton('E_Commerce', 'Users').find_one_by_key_value("email", username_or_email)
     except Exception as e:
         print(str(e))
         return None
     if not dictionary:
-        dictionary = db_manager.find_one_by_key_value("username", username_or_email)
+        MongoDbSingleton.reinitialize()
+        dictionary = MongoDbSingleton('E_Commerce', 'Users').find_one_by_key_value("username", username_or_email)
 
     if not dictionary:
         return jsonify(["Username or email not found!"]), 404
@@ -84,8 +88,7 @@ def edit_payment_details():
     user_id = data["user_id"]
 
     MongoDbSingleton.reinitialize()
-    db_manager = MongoDbSingleton("E_Commerce", "Users")
-    dictionary = db_manager.find_one_by_key_value("_id", user_id)
+    dictionary = MongoDbSingleton('E_Commerce', 'Users').find_one_by_key_value("_id", user_id)
     user = User.from_dict(dictionary)
     payment_instance = PaymentDetails.from_dict(payment_data)
     sent_password = dictionary["password"]
@@ -95,18 +98,18 @@ def edit_payment_details():
                 user.personal_details = PersonalDetails()
                 user.personal_details.payment_details = PaymentDetails()
                 user.personal_details.payment_details = payment_instance
-                db_manager.replace_member(user)
+                MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
                 return jsonify(), 200
             elif user.personal_details is not None and user.personal_details.payment_details is None:
                 user.personal_details.payment_details = PaymentDetails()
                 user.personal_details.payment_details = payment_instance
-                db_manager.replace_member(user)
+                MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
                 return jsonify(), 200
             else:
                 # db_manager.update_member(user_id, 'personal_details.payment_details', payment_instance)
                 user.personal_details.payment_details = PaymentDetails()
                 user.personal_details.payment_details = payment_instance
-                db_manager.replace_member(user)
+                MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
                 return jsonify(), 200
         # try:
         #     db_manager.update_member(user_id, 'personal_details.payment_details', payment_instance)
@@ -119,7 +122,7 @@ def edit_payment_details():
             else:
                 user.personal_details.payment_details = payment_instance
 
-            db_manager.replace_member(user)
+            MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
             return jsonify(), 200
     else:
         jsonify(), 409
@@ -131,16 +134,15 @@ def edit_personal_details():
     personal_data = data["form"]
     user_id = data["user_id"]
     MongoDbSingleton.reinitialize()
-    db_manager = MongoDbSingleton("E_Commerce", "Users")
-    dictionary = db_manager.find_one_by_key_value("_id", user_id)
+    dictionary = MongoDbSingleton('E_Commerce', 'Users').find_one_by_key_value("_id", user_id)
     user = User.from_dict(dictionary)
     address_instance = Address.from_dict(personal_data)
     try:
         if user.personal_details and user.personal_details.address is None:
-            db_manager.update_member(user_id, 'personal_details.address', address_instance)
+            MongoDbSingleton('E_Commerce', 'Users').update_member(user_id, 'personal_details.address', address_instance)
             return jsonify(), 200
         elif user.personal_details and user.personal_details.address:
-            db_manager.update_member(user_id, 'personal_details.address', address_instance)
+            MongoDbSingleton('E_Commerce', 'Users').update_member(user_id, 'personal_details.address', address_instance)
             return jsonify(), 200
         else:
             item_id = None
@@ -164,7 +166,7 @@ def edit_personal_details():
                                                         personal_data['street'], personal_data['number'],
                                                         personal_data['floor'], personal_data['apartment'],
                                                         entrance, mail_box, item_id, created_at)
-        db_manager.replace_member(user)
+        MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
         return jsonify(), 200
     except Exception as e:
         print(str(e))
@@ -174,7 +176,7 @@ def edit_personal_details():
         else:
             user.personal_details.address = address_instance
 
-    db_manager.replace_member(user)
+    MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
     return jsonify(), 200
 
 
@@ -188,8 +190,7 @@ def change_password():
     confirm_new_password = form['confirm_new_password']
 
     MongoDbSingleton.reinitialize()
-    db_manager = MongoDbSingleton("E_Commerce", "Users")
-    dictionary = db_manager.find_one_by_key_value("_id", user_id)
+    dictionary = MongoDbSingleton('E_Commerce', 'Users').find_one_by_key_value("_id", user_id)
     dict_old_password = dictionary["password"]
     if dict_old_password != old_password:
         return jsonify(), 409
@@ -199,17 +200,36 @@ def change_password():
             try:
                 user = User.from_dict(dictionary)
                 user.password = new_password
-                db_manager.replace_member(user)
+                MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
                 # db_manager.update_member(user_id, 'password', new_password)
                 return jsonify(), 200
             except:
                 user = User.from_dict(dictionary)
                 user.password = new_password
-                db_manager.replace_member(user)
+                MongoDbSingleton('E_Commerce', 'Users').replace_member(user)
 
                 return jsonify(), 200
         else:
             jsonify(), 409
+
+
+@app_api.route("/return_personal_details", methods = ["POST"])
+def return_personal_details():
+    data = request.get_json()
+    user_id = data['user_id']
+
+    from DataModels.User import User
+    try:
+        user = User.from_dict(MongoDbSingleton('E_Commerce', 'Users').find_by_id(user_id))
+    except Exception as e:
+        Writer(path=path, file_name=log_file).write_log(str(e), level=LogLevel.ERROR)
+        return jsonify(), 404
+    if not user:
+        return jsonify(), 404
+
+    result = user.personal_details
+
+    return jsonify(result), 200
 
 
 if __name__ == "__main__":
